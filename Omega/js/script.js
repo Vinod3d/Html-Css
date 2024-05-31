455d47d64a0d88acdd37e36abb8c126e5c911d69
Vinod Chandra <vinodchandra979@gmail.com>
2024-05-31 08:04:11 +0530

    Trenline project added

--- Omega/js/script.js
+++
@@ -1,8 +0,0 @@
-
-function toggleNav() {
-    const nav = document.getElementById('main-nav');
-    const hamburger = document.querySelector('.hamburger');
-    
-    nav.classList.toggle('active');
-    hamburger.classList.toggle('active');
-}
